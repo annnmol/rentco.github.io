4c841c90586bcab1a7d06f966b06ca8905e48a4a
Anmol Tanwar <tanwaranmol6@gmail.com>
2022-02-27 11:19:12 +0530

    model

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
+console.log("Hello js");
+
+function headerMenuButtonOnClick() {
+  alert("oops! not designed yet");
+}
 
 
-console.log("hello");
+function search() {
+  alert("oops! search button not designed yet");
+}
+
+function signupbutton() {
+  let username = document.getElementById("username").value;
+  let password = document.getElementById("password").value;
+
+    let userRecords= new Array();
+    userRecords=JSON.parse(localStorage.getItem("userids"))?JSON.parse(localStorage.getItem("userids")):[]
+    
+    userRecords.push({
+        "username is":username,
+        "password is ":password,
+    })
+    
+    localStorage.setItem("userids",JSON.stringify(userRecords));
+    console.log(userRecords);
+  alert("details saved in local storage");
+}
 
 const hamburger=document.querySelector(".hamburger");
 const navMenu=document.querySelector(".nav-menu");
@@ -10,37 +34,74 @@ hamburger.addEventListener("click", ()=>{
     navMenu.classList.toggle("active");
 })
 
-
 document.querySelectorAll(".nav-link").forEach(n => n.
     addEventListener ("click", () => {
         hamburger.classList.remove("active");
        navMenu. classList.remove("active");}))
 
 
-       const submitGo=document.querySelector('.submitgo');
+let jDate = document.getElementById("jDate");
+let rDate = document.getElementById("rDate");
 
-       submitGo.addEventListener('click',()=>{
-        var pickup = document.getElementById('pickup').value;  
-           alert(`thnx for searching! we'll update you soon-- Your Pickup Address is: ${pickup} `);
-            
-       })
+function change1(e) {
+    jDate = e.target.value;
+    // alert(e.target.value);
+}
+
+function change2(e) {
+    rDate = e.target.value;
+    //   console.log(rDate);
+    // alert(e.target.value);
+}
+
+function savedata() {
+
+    //   localStorage.clear();
+
+    let pickup = document.getElementById("pickup").value;
+    let destination = document.getElementById("destination").value;
+
+    // console.log(`Pickup address is: ${pickup}`);
+    // console.log(`Destination address is: ${destination}`);
+    // console.log(`Travel date is: ${jDate}`);
+    // console.log(`Return date is: ${rDate}`);
+
+    //   localStorage.setItem("Pickup Addess", pickup);
+    // localStorage.setItem("Destination Address", destination);
+    // localStorage.setItem("Journey Date", jDate);
+    // localStorage.setItem("Return Date", rDate);
+
+    let search_records = new Array();
+      search_records=JSON.parse(localStorage.getItem("userids"))?JSON.parse(localStorage.getItem("userids")):[];
+
+    search_records.push({
+        "pickup Address is" : pickup,
+        "destination Address is" : destination,
+        "Journey date is" : jDate,
+        "Return date is" : rDate,
+    })
+
+    localStorage.setItem("userids", JSON.stringify(search_records));
+    console.log(search_records);
+    alert("search details saved in local storage");
+
+}
+
+function saveemail() {
+    //   localStorage.clear();
+    let email = document.getElementById("email").value;
+
+    let emailRecords= new Array();
+    emailRecords=JSON.parse(localStorage.getItem("emails"))?JSON.parse(localStorage.getItem("emails")):[]
+    
+    emailRecords.push({
+        "emails are ":email,
+    })
+    
+    localStorage.setItem("emails",JSON.stringify(emailRecords));
+    console.log(emailRecords);
+    alert("you're now added to newsletter list and details saved in local storage");
+}
 
 
-function save() {
-    alert("oops! not designed yet");
-  }
-
-  function signupbutton() {
-    alert("oops! login form  not designed yet");
-  }
-  function search() {
-    alert("oops! search button not designed yet");
-    // alert("oops! login form  not designed yet");
-  }
-
-const footerSubmit=document.querySelector('.footersubmit');
-
-footerSubmit.addEventListener('click',()=>{
-    alert("you're now added to weekly email list");
-})
 
